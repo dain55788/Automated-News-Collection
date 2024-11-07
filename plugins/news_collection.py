9405ff2b091fd165af5d63c8f9553f50666d4220
dain55788 <dain55788@gmail.com>
2024-11-07 11:19:24 +0700

    Updated

--- plugins/news_collection.py
+++ plugins/news_collection.py
@@ -5,11 +5,12 @@ from data_transformation import transform
 # DATA EXTRACTION PHASE
 # INIT
 file_path = 'Automated-News-Collection/news_data/news.json'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
 
 # ! Data Transformation included
 def get_news_data():
     url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=4b3891226ba640f2923d70515226148b"
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
 
     print("Start execution")
 
@@ -18,7 +19,7 @@ def get_news_data():
         news = response.json()
         
         # Save the data to a JSON file
-        with open(file_path, 'w') as f:
+        with open(file_path, 'w', encoding='utf-8') as f:
             json.dump(news, f, ensure_ascii=False, indent=4)
         
         transform(file_path)
